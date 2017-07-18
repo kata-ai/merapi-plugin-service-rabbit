13e5a942c98bb2f36d45224cd6458561dec6e778
Ikmal Syifai <ikmal.s.16@gmail.com>
2017-07-18 16:18:55 +0700

    Plugin index

--- index.js
+++ index.js
@@ -9,14 +9,21 @@ module.exports = function () {
             container.register("servicePubRabbit", require("./lib/service_pub_rabbit"));
             container.register("serviceSubRabbit", require("./lib/service_sub_rabbit"));
             container.alias("serviceSub", "serviceSubRabbit");
-            
+
+            container.register("servicePubQueueRabbit", require("./lib/service_pub_queue_rabbit"));
+            container.register("serviceSuQueuebRabbit", require("./lib/service_sub_queue_rabbit"));
+            container.alias("servicePubQueue", "servicePubQueueRabbit");
+
             container.register("amqp", require("./lib/service_amqp"));
         },
         *onInit(container) {
             let service = yield container.resolve("service");
-            let servicePubRabbit = yield container.resolve("servicePubRabbit");
 
+            let servicePubRabbit = yield container.resolve("servicePubRabbit");
             service.addModule("pub-rabbit", servicePubRabbit);
+
+            let serviceSubQueueRabbit = yield container.resolve("serviceSubQueueRabbit");
+            service.addModule("sub-queue-rabbit", serviceSubQueueRabbit);
         }
     };
 };
