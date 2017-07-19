051ee0b2763fd7102a4f31692b46806c39bb6712
Ikmal Syifai <ikmal.s.16@gmail.com>
2017-07-19 11:24:10 +0700

    Migration to github

--- index.js
+++ index.js
@@ -3,20 +3,22 @@
 module.exports = function () {
     return {
         dependencies: [
-            "service@yesboss"
+            "service"
         ],
         *onBeforeComponentsRegister(container) {
             container.register("servicePubRabbit", require("./lib/service_pub_rabbit"));
             container.register("serviceSubRabbit", require("./lib/service_sub_rabbit"));
             container.alias("serviceSub", "serviceSubRabbit");
-            
+
             container.register("amqp", require("./lib/service_amqp"));
         },
         *onInit(container) {
             let service = yield container.resolve("service");
             let servicePubRabbit = yield container.resolve("servicePubRabbit");
+            let serviceSubRabbit = yield container.resolve("serviceSubRabbit");
 
             service.addModule("pub-rabbit", servicePubRabbit);
+            service.addModule("sub-rabbit", serviceSubRabbit);
         }
     };
 };
