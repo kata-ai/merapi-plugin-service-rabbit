929fe9fd941838f5ac0668a653c93c36b7922ceb
Reyhan Sofian Haqqi <reyhan@kata.ai>
2018-10-02 14:45:45 +0700

    test: fix test

--- test/service_pub_queue_rabbit_test.js
+++ test/service_pub_queue_rabbit_test.js
@@ -1,158 +1,181 @@
-"use strict";
+'use strict';
 
-const chai = require("chai");
+const chai = require('chai');
 const expect = chai.expect;
-const request = require("supertest");
-const amqplib = require("amqplib");
-const sleep = require("then-sleep");
+const request = require('supertest');
+const amqplib = require('amqplib');
+const sleep = require('then-sleep');
 
-const merapi = require("merapi");
-const { Component, async } = require("merapi");
+const merapi = require('merapi');
+const { Component, async } = require('merapi');
 
 /* eslint-env mocha */
 
-describe("Merapi Plugin Service: Queue Publisher", function () {
-    let publisherAContainer, publisherBContainer;
-    let service = {};
-    let connection = {};
-    let channel = {};
-    let currentIteration = 1;
-    const expireTime = 1000 * 30;
+describe('Merapi Plugin Service: Queue Publisher', function() {
+  let publisherAContainer, publisherBContainer;
+  let service = {};
+  let connection = {};
+  let channel = {};
+  let currentIteration = 1;
 
-    beforeEach(async(function* () {
-        this.timeout(5000);
+  beforeEach(async(function*() {
+    this.timeout(5000);
 
-        let publisherConfig = {
-            name: "publisher",
-            version: "1.0.0",
-            main: "mainCom",
-            secret: "abc123",
-            plugins: [
-                "service"
-            ],
-            service: {
-                "rabbit": {
-                    "host": "localhost",
-                    "port": 5672,
-                    "expireTime": expireTime,
-                },
-                "queue": {
-                    "publish": {
-                        "subscriber": {
-                            "in_queue_publisher_test": "inQueuePublisherTest",
-                            "out_queue_publisher_test": "outQueuePublisherTest"
-                        }
-                    }
-                }
-            }
-        };
+    let publisherConfig = {
+      name: 'publisher',
+      version: '1.0.0',
+      main: 'mainCom',
+      secret: 'abc123',
+      plugins: ['service'],
+      service: {
+        rabbit: {
+          host: 'localhost',
+          port: 5672,
+        },
+        queue: {
+          publish: {
+            subscriber: {
+              in_queue_publisher_test: 'inQueuePublisherTest',
+              out_queue_publisher_test: 'outQueuePublisherTest',
+            },
+          },
+        },
+      },
+    };
 
-        publisherConfig.service.port = 5300 + currentIteration;
-        publisherAContainer = merapi({
-            basepath: __dirname,
-            config: publisherConfig
-        });
-
-        publisherAContainer.registerPlugin("service-rabbit", require("../index.js")(publisherAContainer));
-        publisherAContainer.register("mainCom", class MainCom extends Component { start() { } });
-        yield publisherAContainer.start();
-
-        publisherConfig.service.port = 5400 + currentIteration;
-        publisherBContainer = merapi({
-            basepath: __dirname,
-            config: publisherConfig
-        });
-
-        publisherBContainer.registerPlugin("service-rabbit", require("../index.js")(publisherBContainer));
-        publisherBContainer.register("mainCom", class MainCom extends Component { start() { } });
-        yield publisherBContainer.start();
-
-        service = yield publisherAContainer.resolve("service");
-        connection = yield amqplib.connect("amqp://localhost");
-        channel = yield connection.createChannel();
-
-        yield sleep(100);
-    }));
-
-    afterEach(async(function* () {
-        yield publisherAContainer.stop();
-        yield channel.close();
-        yield connection.close();
-
-        currentIteration++;
-    }));
-
-    describe("Publisher Queue service", function () {
-
-        describe("info", function () {
-            it("should list pub-queue-rabbit", async(function* () {
-                yield request(service._express)
-                    .get("/info")
-                    .expect(function (res) {
-                        expect(Object.keys(res.body.modules).some(key => key == "pub-queue-rabbit")).to.be.true;
-                    });
-            }));
-        });
-
-        describe("when initializing", function () {
-            it("should resolve trigger components", async(function* () {
-                let triggerA = yield publisherAContainer.resolve("inQueuePublisherTest");
-                let triggerB = yield publisherAContainer.resolve("outQueuePublisherTest");
-                expect(triggerA).to.not.be.null;
-                expect(triggerB).to.not.be.null;
-            }));
-
-            it("should create queue", function () {
-                expect(async(function* () {
-                    yield channel.checkQueue("default.queue.subscriber.in_queue_publisher_test");
-                    yield channel.checkQueue("default.queue.subscriber.out_queue_publisher_test");
-                })).to.not.throw(Error);
-            });
-        });
-
-        describe("when publishing event", function () {
-            let q, payload, triggerA, triggerB;
-
-            it("should publish event to queue", async(function* () {
-                q = yield channel.assertQueue(
-                    "default.queue.subscriber.in_queue_publisher_test",
-                    { expires: expireTime }
-                );
-
-                triggerA = yield publisherAContainer.resolve("inQueuePublisherTest");
-                payload = { key: "value" };
-                yield triggerA(payload);
-
-                channel.consume(q.queue, function (msg) {
-                    expect(msg.content.toString()).to.equal(JSON.stringify(payload));
-                    channel.ack(msg);
-                });
-            }));
-
-            it("should publish events to the same queue for same service", async(function* () {
-                q = yield channel.assertQueue(
-                    "default.queue.subscriber.out_queue_publisher_test",
-                    { expires: expireTime }
-                );
-
-                triggerA = yield publisherAContainer.resolve("outQueuePublisherTest");
-                triggerB = yield publisherBContainer.resolve("outQueuePublisherTest");
-
-                for (let i = 0; i < 5; i++) {
-                    if (i % 2 == 0) yield triggerA(i); else yield triggerB(i);
-                    yield sleep(150);
-                }
-
-                let message = [];
-                channel.consume(q.queue, function (msg) {
-                    message.push(msg.content.toString());
-                    channel.ack(msg);
-                });
-
-                yield sleep(1000);
-                expect(message).to.deep.equal(["0", "1", "2", "3", "4"]);
-            }));
-        });
+    publisherConfig.service.port = 5300 + currentIteration;
+    publisherAContainer = merapi({
+      basepath: __dirname,
+      config: publisherConfig,
     });
 
+    publisherAContainer.registerPlugin(
+      'service-rabbit',
+      require('../index.js')(publisherAContainer)
+    );
+    publisherAContainer.register(
+      'mainCom',
+      class MainCom extends Component {
+        start() {}
+      }
+    );
+    yield publisherAContainer.start();
+
+    publisherConfig.service.port = 5400 + currentIteration;
+    publisherBContainer = merapi({
+      basepath: __dirname,
+      config: publisherConfig,
+    });
+
+    publisherBContainer.registerPlugin(
+      'service-rabbit',
+      require('../index.js')(publisherBContainer)
+    );
+    publisherBContainer.register(
+      'mainCom',
+      class MainCom extends Component {
+        start() {}
+      }
+    );
+    yield publisherBContainer.start();
+
+    service = yield publisherAContainer.resolve('service');
+    connection = yield amqplib.connect('amqp://localhost');
+    channel = yield connection.createChannel();
+
+    yield sleep(100);
+  }));
+
+  afterEach(async(function*() {
+    yield publisherAContainer.stop();
+    yield channel.close();
+    yield connection.close();
+
+    currentIteration++;
+  }));
+
+  describe('Publisher Queue service', function() {
+    describe('info', function() {
+      it('should list pub-queue-rabbit', async(function*() {
+        yield request(service._express)
+          .get('/info')
+          .expect(function(res) {
+            expect(
+              Object.keys(res.body.modules).some(
+                key => key == 'pub-queue-rabbit'
+              )
+            ).to.be.true;
+          });
+      }));
+    });
+
+    describe('when initializing', function() {
+      it('should resolve trigger components', async(function*() {
+        let triggerA = yield publisherAContainer.resolve(
+          'inQueuePublisherTest'
+        );
+        let triggerB = yield publisherAContainer.resolve(
+          'outQueuePublisherTest'
+        );
+        expect(triggerA).to.not.be.null;
+        expect(triggerB).to.not.be.null;
+      }));
+
+      it('should create queue', function() {
+        expect(
+          async(function*() {
+            yield channel.checkQueue(
+              'default.queue.subscriber.in_queue_publisher_test'
+            );
+            yield channel.checkQueue(
+              'default.queue.subscriber.out_queue_publisher_test'
+            );
+          })
+        ).to.not.throw(Error);
+      });
+    });
+
+    describe('when publishing event', function() {
+      let q, payload, triggerA, triggerB;
+
+      it('should publish event to queue', async(function*() {
+        q = yield channel.assertQueue(
+          'default.queue.subscriber.in_queue_publisher_test'
+        );
+
+        triggerA = yield publisherAContainer.resolve('inQueuePublisherTest');
+        payload = { key: 'value' };
+        yield triggerA(payload);
+
+        channel.consume(q.queue, function(msg) {
+          expect(msg.content.toString()).to.equal(JSON.stringify(payload));
+          channel.ack(msg);
+        });
+      }));
+
+      it('should publish events to the same queue for same service', async(function*() {
+        q = yield channel.assertQueue(
+          'default.queue.subscriber.out_queue_publisher_test'
+        );
+
+        triggerA = yield publisherAContainer.resolve('outQueuePublisherTest');
+        triggerB = yield publisherBContainer.resolve('outQueuePublisherTest');
+
+        for (let i = 0; i < 5; i++) {
+          if (i % 2 == 0) yield triggerA(i);
+          else yield triggerB(i);
+          yield sleep(150);
+        }
+
+        let message = [];
+        channel.consume(q.queue, function(msg) {
+          message.push(msg.content.toString());
+          channel.ack(msg);
+        });
+
+        yield sleep(1000);
+        expect(message).to.deep.equal(['0', '1', '2', '3', '4']);
+      }));
+    });
+  });
 });
--- test/service_pub_rabbit_test.js
+++ test/service_pub_rabbit_test.js
@@ -1,165 +1,198 @@
-"use strict";
+'use strict';
 
-const chai = require("chai");
+const chai = require('chai');
 const expect = chai.expect;
-const request = require("supertest");
-const amqplib = require("amqplib");
-const sleep = require("then-sleep");
-const chaiAsPromised = require("chai-as-promised");
+const request = require('supertest');
+const amqplib = require('amqplib');
+const sleep = require('then-sleep');
+const chaiAsPromised = require('chai-as-promised');
 
-const merapi = require("merapi");
-const { Component, async } = require("merapi");
+const merapi = require('merapi');
+const { Component, async } = require('merapi');
 
 chai.use(chaiAsPromised);
 
 /* eslint-env mocha */
 
-describe("Merapi Plugin Service: Publisher", function () {
-    let publisherAContainer, publisherBContainer;
-    let service = {};
-    let connection = {};
-    let channel = {};
-    let currentIteration = 1;
-    const expires = 1000 * 30;
+describe('Merapi Plugin Service: Publisher', function() {
+  let publisherAContainer, publisherBContainer;
+  let service = {};
+  let connection = {};
+  let channel = {};
+  let currentIteration = 1;
 
-    this.timeout(5000);
+  this.timeout(5000);
 
-    beforeEach(async(function* () {
+  beforeEach(async(function*() {
+    let publisherConfig = {
+      name: 'publisher',
+      version: '1.0.0',
+      main: 'mainCom',
+      secret: 'abc123',
+      plugins: ['service'],
+      service: {
+        rabbit: {
+          host: 'localhost',
+          port: 5672,
+        },
+        publish: {
+          incoming_message_publisher_test:
+            'triggerIncomingMessagePublisherTest',
+          outgoing_message_publisher_test:
+            'triggerOutgoingMessagePublisherTest',
+        },
+      },
+    };
 
-        let publisherConfig = {
-            name: "publisher",
-            version: "1.0.0",
-            main: "mainCom",
-            secret: "abc123",
-            plugins: [
-                "service"
-            ],
-            service: {
-                "rabbit": {
-                    "host": "localhost",
-                    "port": 5672,
-                },
-                "publish": {
-                    "incoming_message_publisher_test": "triggerIncomingMessagePublisherTest",
-                    "outgoing_message_publisher_test": "triggerOutgoingMessagePublisherTest"
-                }
-            }
-        };
-
-        publisherConfig.service.port = 5700 + currentIteration;
-        publisherAContainer = merapi({
-            basepath: __dirname,
-            config: publisherConfig
-        });
-
-        publisherAContainer.registerPlugin("service-rabbit", require("../index.js")(publisherAContainer));
-        publisherAContainer.register("mainCom", class MainCom extends Component { start() { } });
-        yield publisherAContainer.start();
-
-        publisherConfig.service.port = 5800 + currentIteration;
-        publisherBContainer = merapi({
-            basepath: __dirname,
-            config: publisherConfig
-        });
-
-        publisherBContainer.registerPlugin("service-rabbit", require("../index.js")(publisherBContainer));
-        publisherBContainer.register("mainCom", class MainCom extends Component { start() { } });
-        yield publisherBContainer.start();
-
-        service = yield publisherAContainer.resolve("service");
-        connection = yield amqplib.connect("amqp://localhost");
-        channel = yield connection.createChannel();
-
-        yield sleep(100);
-    }));
-
-    afterEach(async(function* () {
-        yield publisherAContainer.stop();
-        yield publisherBContainer.stop();
-        yield channel.close();
-        yield connection.close();
-
-        currentIteration++;
-    }));
-
-    describe("Publisher service", function () {
-
-        describe("info", function () {
-            it("should list pub-rabbit", async(function* () {
-                yield request(service._express)
-                    .get("/info")
-                    .expect(function (res) {
-                        expect(Object.keys(res.body.modules).some(key => key == "pub-rabbit")).to.be.true;
-                    });
-            }));
-        });
-
-        describe("when initializing", function () {
-            it("should resolve triggerIncomingMessagePublisherTest", async(function* () {
-                let trigger = yield publisherAContainer.resolve("triggerIncomingMessagePublisherTest");
-                expect(trigger).to.not.be.null;
-            }));
-
-            it("should save event list", function () {
-                let servicePubRabbit = service.getModule("pub-rabbit");
-                let expectedPubRabbit = ["incoming_message_publisher_test", "outgoing_message_publisher_test"];
-                expect(servicePubRabbit.getEventList()).to.deep.equal(expectedPubRabbit);
-            });
-
-            it("should create exchanges", function () {
-                expect(async(function* () {
-                    yield channel.checkExchange("default.publisher.incoming_message_publisher_test");
-                    yield channel.checkExchange("default.publisher.outgoing_message_publisher_test");
-                })).to.not.throw(Error);
-            });
-        });
-
-        describe("when publishing event", function () {
-            let q, exchangeName, payload, triggerA, triggerB;
-
-            it("should publish event to exchange", async(function* () {
-                q = yield channel.assertQueue("default.queue1", { expires });
-                payload = { key: "value" };
-                triggerA = yield publisherAContainer.resolve("triggerIncomingMessagePublisherTest");
-                exchangeName = "default.publisher.incoming_message_publisher_test";
-                yield triggerA(payload);
-
-                yield channel.bindQueue(q.queue, exchangeName, "");
-                channel.consume(q.queue, function (msg) {
-                    expect(msg.content.toString()).to.equal(JSON.stringify(payload));
-                    channel.ack(msg);
-                });
-            }));
-
-            it("should publish events to the same exchange for same service", async(function* () {
-                let message = [];
-
-                q = yield channel.assertQueue("default.queue2", { expires });
-                triggerA = yield publisherAContainer.resolve("triggerOutgoingMessagePublisherTest");
-                triggerB = yield publisherBContainer.resolve("triggerOutgoingMessagePublisherTest");
-                exchangeName = "default.publisher.outgoing_message_publisher_test";
-
-                for (let i = 0; i < 5; i++) {
-                    yield sleep(150);
-                    if (i % 2 == 0) {
-                        yield triggerA(i);
-                    }  else {
-                        yield triggerB(i);
-                    }
-                }
-
-                yield channel.bindQueue(q.queue, exchangeName, "");
-
-                channel
-                    .consume(q.queue, function (msg) {
-                        message.push(msg.content.toString());
-                        channel.ack(msg);
-                    })
-                    .then(function () {
-                        expect(message).to.deep.equal(["0", "1", "2", "3", "4"]);
-                    });
-            }));
-        });
+    publisherConfig.service.port = 5700 + currentIteration;
+    publisherAContainer = merapi({
+      basepath: __dirname,
+      config: publisherConfig,
     });
 
+    publisherAContainer.registerPlugin(
+      'service-rabbit',
+      require('../index.js')(publisherAContainer)
+    );
+    publisherAContainer.register(
+      'mainCom',
+      class MainCom extends Component {
+        start() {}
+      }
+    );
+    yield publisherAContainer.start();
+
+    publisherConfig.service.port = 5800 + currentIteration;
+    publisherBContainer = merapi({
+      basepath: __dirname,
+      config: publisherConfig,
+    });
+
+    publisherBContainer.registerPlugin(
+      'service-rabbit',
+      require('../index.js')(publisherBContainer)
+    );
+    publisherBContainer.register(
+      'mainCom',
+      class MainCom extends Component {
+        start() {}
+      }
+    );
+    yield publisherBContainer.start();
+
+    service = yield publisherAContainer.resolve('service');
+    connection = yield amqplib.connect('amqp://localhost');
+    channel = yield connection.createChannel();
+
+    yield sleep(100);
+  }));
+
+  afterEach(async(function*() {
+    yield publisherAContainer.stop();
+    yield publisherBContainer.stop();
+    yield channel.close();
+    yield connection.close();
+
+    currentIteration++;
+  }));
+
+  describe('Publisher service', function() {
+    describe('info', function() {
+      it('should list pub-rabbit', async(function*() {
+        yield request(service._express)
+          .get('/info')
+          .expect(function(res) {
+            expect(
+              Object.keys(res.body.modules).some(key => key == 'pub-rabbit')
+            ).to.be.true;
+          });
+      }));
+    });
+
+    describe('when initializing', function() {
+      it('should resolve triggerIncomingMessagePublisherTest', async(function*() {
+        let trigger = yield publisherAContainer.resolve(
+          'triggerIncomingMessagePublisherTest'
+        );
+        expect(trigger).to.not.be.null;
+      }));
+
+      it('should save event list', function() {
+        let servicePubRabbit = service.getModule('pub-rabbit');
+        let expectedPubRabbit = [
+          'incoming_message_publisher_test',
+          'outgoing_message_publisher_test',
+        ];
+        expect(servicePubRabbit.getEventList()).to.deep.equal(
+          expectedPubRabbit
+        );
+      });
+
+      it('should create exchanges', function() {
+        expect(
+          async(function*() {
+            yield channel.checkExchange(
+              'default.publisher.incoming_message_publisher_test'
+            );
+            yield channel.checkExchange(
+              'default.publisher.outgoing_message_publisher_test'
+            );
+          })
+        ).to.not.throw(Error);
+      });
+    });
+
+    describe('when publishing event', function() {
+      let q, exchangeName, payload, triggerA, triggerB;
+
+      it('should publish event to exchange', async(function*() {
+        q = yield channel.assertQueue('default.queue1');
+        payload = { key: 'value' };
+        triggerA = yield publisherAContainer.resolve(
+          'triggerIncomingMessagePublisherTest'
+        );
+        exchangeName = 'default.publisher.incoming_message_publisher_test';
+        yield triggerA(payload);
+
+        yield channel.bindQueue(q.queue, exchangeName, '');
+        channel.consume(q.queue, function(msg) {
+          expect(msg.content.toString()).to.equal(JSON.stringify(payload));
+          channel.ack(msg);
+        });
+      }));
+
+      it('should publish events to the same exchange for same service', async(function*() {
+        let message = [];
+
+        q = yield channel.assertQueue('default.queue2');
+        triggerA = yield publisherAContainer.resolve(
+          'triggerOutgoingMessagePublisherTest'
+        );
+        triggerB = yield publisherBContainer.resolve(
+          'triggerOutgoingMessagePublisherTest'
+        );
+        exchangeName = 'default.publisher.outgoing_message_publisher_test';
+
+        for (let i = 0; i < 5; i++) {
+          yield sleep(150);
+          if (i % 2 == 0) {
+            yield triggerA(i);
+          } else {
+            yield triggerB(i);
+          }
+        }
+
+        yield channel.bindQueue(q.queue, exchangeName, '');
+
+        channel
+          .consume(q.queue, function(msg) {
+            message.push(msg.content.toString());
+            channel.ack(msg);
+          })
+          .then(function() {
+            expect(message).to.deep.equal(['0', '1', '2', '3', '4']);
+          });
+      }));
+    });
+  });
 });
